52ecd4d92bd02aa8d0cda217a18307a4a13deb75
Bz14 <eyerusalembezu15@gmail.com>
2024-06-30 15:46:50 +0000

    File updated.

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -4,7 +4,7 @@ file { '/etc/ssh/ssh_config':
   ensure  => 'present',
 }
 
-file_line {'Turn off passwd auth':
+file_line { 'Turn off passwd auth':
   path    => '/etc/ssh/ssh_config',
   line    => 'PasswordAuthentication no',
   match   => 'PasswordAuthentication yes',
