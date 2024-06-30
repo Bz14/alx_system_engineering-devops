689180b669990226b31fb62d6e6e3c352926b8bd
Bz14 <eyerusalembezu15@gmail.com>
2024-06-30 15:33:42 +0000

    Client configuration file

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+#Client configuration file
+file { '/etc/ssh/ssh_config':
+  ensure  => 'present',
+}
+
+file_line {'Turn off passwd auth':
+  path    => '/etc/ssh/ssh_config',
+  line    => 'PasswordAuthentication no',
+  match   => 'PasswordAuthentication yes',
+  replace => 'true',
+}
+
+file_line {'Use a Identityfile':
+  ensure  => 'present',
+  path    => '/etc/ssh/ssh_config',
+  line    => 'IdentifyFile ~/.ssh/config',
+  match   => '^IdentifyFile',
+}
