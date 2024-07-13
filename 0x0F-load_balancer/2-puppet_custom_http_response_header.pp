30d1226fb99a22b681afc8e9f303f2d2bc61af19
Bz14 <eyerusalembezu15@gmail.com>
2024-07-13 14:27:10 +0000

    File updated.

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,30 +1,9 @@
 # Setup New Ubuntu server with nginx
 
-exec { 'update system':
-        command => '/usr/bin/apt-get update',
-}
-
-package { 'nginx':
-	ensure => 'installed',
-	require => Exec['update system']
-}
-
-file {'/var/www/html/index.html':
-     content => 'Hello World!'
-}
-
 exec { 'add_custom_header':
-  command  => 'sed -i "/server_name _;/a \\        add_header X-Served-By \$hostname;" /etc/nginx/sites-available/default',
-  provider => 'shell',
-  require  => Package['nginx'],
-}
-
-exec {'redirect_me':
-     command => 'sed -i "24i\	rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default',
-     provider => 'shell'
-}
-
-service {'nginx':
-	ensure => running,
-	require => Package['nginx']
+  command  => 'sudo apt-get update -y;
+  sudo apt-get install nginx -y;
+  sudo sed -i "/server_name _/a add_header X-Served-By HOSTNAME;" /etc/nginx/sites-available/default
+  sudo service nginx restart',
+  provider => shell,
 }
