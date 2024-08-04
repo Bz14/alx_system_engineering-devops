436fdc6db829e4e826467e742108df0602312464
root <root@DESKTOP-EOIAI1O>
2024-08-04 18:12:27 +0300

    Gather data from an API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,11 +1,24 @@
 #!/usr/bin/python3
 """ A module to gather data from an API"""
 import requests
+import sys
 
 
 if __name__ == "__main__":
-    url = "https://jsonplaceholder.typicode.com"
-    response = request.get(url)
+    id = sys.argv[1]
+    employee_url = "https://jsonplaceholder.typicode.com/users/{}".format(id)
+    url = "https://jsonplaceholder.typicode.com/todos?userId={}".format(id)
+    response = requests.get(employee_url)
     if response.status_code == 200:
-        data = responce.json()
-        print(data)
+        data = response.json()
+        name = data.get("name")
+        response = requests.get(url)
+        if response.status_code == 200:
+            todos = response.json()
+            t = len(todos)
+            completed_task = [task for task in todos if task.get("completed")]
+            n = len(completed_task)
+            st = "Employee {} is done with tasks ({}/{}):".format(name, n, t)
+            print(st)
+            for task in completed_task:
+                print("\t {}".format(task.get("title")))
