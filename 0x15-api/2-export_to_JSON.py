919fe74e47d3bfb2f3bba4abc46e46cdb6cc007b
root <root@DESKTOP-EOIAI1O>
2024-08-04 20:04:25 +0300

    Export to json

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+""" A module to gather data from an API"""
+import json
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    id = sys.argv[1]
+    employee_url = "https://jsonplaceholder.typicode.com/users/{}".format(id)
+    url = "https://jsonplaceholder.typicode.com/todos?userId={}".format(id)
+    response = requests.get(employee_url)
+    if response.status_code == 200:
+        data = response.json()
+        username = data.get("username")
+        response = requests.get(url)
+        if response.status_code == 200:
+            todos = response.json()
+            json_data = []
+            res = []
+            for task in todos:
+                completed = task.get("completed")
+                title = task.get("title")
+                res.append({
+                        "task": title,
+                        "completed": completed,
+                        "username": username
+                })
+            json_data.append({
+                id: res
+            })
+            file = "{}.json".format(id)
+            with open(file, mode="w", encoding='utf-8') as f:
+                json.dump(json_data, f)
