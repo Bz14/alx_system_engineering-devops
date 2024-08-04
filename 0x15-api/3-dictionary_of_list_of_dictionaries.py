65bab6c4d5e0f1f0c92c33d9494bf00596cf239f
root <root@DESKTOP-EOIAI1O>
2024-08-04 20:32:04 +0300

    Dictionary of list of dictionaries

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+""" A module to gather data from an API"""
+import json
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    employee_url = "https://jsonplaceholder.typicode.com/users"
+    url = "https://jsonplaceholder.typicode.com/todos?userId={}"
+    json_data = {}
+    response = requests.get(employee_url)
+    if response.status_code == 200:
+        data = response.json()
+        for user in data:
+            username = user.get("username")
+            id = user.get("id")
+            url = url.format(id)
+            response = requests.get(url)
+            if response.status_code == 200:
+                todos = response.json()
+                res = []
+                for task in todos:
+                    completed = task.get("completed")
+                    title = task.get("title")
+                    res.append({
+                        "task": title,
+                        "completed": completed,
+                        "username": username
+                    })
+                json_data[id] = res
+                file = "todo_all_employees.json"
+                with open(file, mode="w", encoding='utf-8') as f:
+                    json.dump(json_data, f)
